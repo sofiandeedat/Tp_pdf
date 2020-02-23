eec9096ff3a7be610cbae7eb4fd2ca42c90ae4fa
SofianD <sofianmrc@gmail.com>
2020-02-23 15:26:06 +0100

    mon premier

---
+++ src/main/java/com/ensat/controllers/ProductController.java
@@ -0,0 +1,112 @@
+package com.ensat.controllers;
+
+import com.ensat.entities.Product;
+import com.ensat.services.ProductService;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsPdfView;
+
+/**
+ * Product controller.
+ */
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+    @Autowired
+    private org.springframework.context.ApplicationContext applicationContext;
+    /**
+     * List all products.
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("products", productService.listAllProducts());
+        System.out.println("Returning rpoducts:");
+        return "products";
+    }
+
+    /**
+     * View a specific product by its id.
+     *
+     * @param id
+     * @param model
+     * @return
+     */
+    @RequestMapping("product/{id}")
+    public String showProduct(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "productshow";
+    }
+
+    // Afficher le formulaire de modification du Product
+    @RequestMapping("product/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "productform";
+    }
+
+    /**
+     * New product.
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping("product/new")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "productform";
+    }
+
+    /**
+     * Save product to database.
+     *
+     * @param product
+     * @return
+     */
+    @RequestMapping(value = "product", method = RequestMethod.POST)
+    public String saveProduct(Product product) {
+        productService.saveProduct(product);
+        return "redirect:/product/" + product.getId();
+    }
+    
+    
+    
+  
+    @RequestMapping(value = "product/report1", method = RequestMethod.GET)
+    public ModelAndView report1() {
+        JasperReportsPdfView view = new JasperReportsPdfView();
+        view.setUrl("classpath:/templates/rpt_users.jrxml");
+        view.setApplicationContext(applicationContext);
+		
+    }
+    
+    
+
+    /**
+     * Delete product by its id.
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping("product/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        productService.deleteProduct(id);
+        return "redirect:/products";
+    }
+
+}
---
+++ src/test/java/testU/testUnitaire.java
@@ -0,0 +1,14 @@
+package testU;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testUnitaire {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
